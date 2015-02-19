3ff4af73451ea37fcf1e8041cd5d972aa04ad9c1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-02-19 13:57:02 +0100

    refactor: move bootstrap logic to index.html

--- src/hello.js
+++ src/hello.js
@@ -1,21 +1,16 @@
-import {bootstrap, Component, Template} from 'angular2/core';
+import {Component, Template} from 'angular2/core';
 
 @Component({
-    selector: 'hello-app'  //TODO: default to camel-cased class name if not provided?
+    selector: 'hello'  //TODO: default to camel-cased class name if not provided?
 })
 @Template({
     inline: `<span>Hello, {{name}}!</span>`
 })
-class HelloApp {
+export class Hello {
     constructor() {
         this.name = 'World'; //TODO: how to declare a field with its default value (so I don't need a constructor)?
         setTimeout(() => {
           this.name = 'NEW World'
         }, 2000);
     }
-}
-
-//TODO: maybe we could use sth like <div ng-app="app::AppCmpt"></div> or similar?
-export function main() {
-    bootstrap(HelloApp);
 }
