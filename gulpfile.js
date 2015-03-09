12b3e15f12f7476b3f216fe4629787118edf5689
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-03-09 16:07:51 +0100

    chore: use zone.js from angular dist

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var PATHS = {
         'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
         'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
         'node_modules/systemjs/lib/extension-register.js',
-        'node_modules/zone.js/zone.js'
+        'node_modules/angular2/node_modules/zone.js/zone.js'
     ]
 };
 
