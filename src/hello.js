3071c1bc08cdac2cca8cfef7019ba9dfb3bfe0c9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-04-03 15:49:29 +0200

    refactor: move bootstrap logic to JS
    Closes #8

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,6 @@
 import {Component, Template} from 'angular2/angular2';
 import {If} from 'angular2/angular2';
+import {bootstrap} from 'angular2/angular2';
 
 @Component({
     selector: 'hello'  //TODO: default to camel-cased class name if not provided?
@@ -16,3 +17,5 @@ export class Hello {
         }, 2000);
     }
 }
+
+bootstrap(Hello);
