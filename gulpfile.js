6335240e5f39fa87b7b3678d5059e4cc25d8dbd3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-04-05 14:24:00 +0200

    feat: switch angular2 build to systemjs-builder
    Starting from https://github.com/angular/angular/commit/c0b04ca0bc762bc925434a8762eee74ce999e57e
    Angular2 is switching to systemjs-builder for its one-file packaging.
    
    This change alligns with this move. It allows us to limit number of files
    to be downloaded into a browser (rx.js gets bundeld with angular2)

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,9 @@ var PATHS = {
     lib: [
       'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
       'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
-      'node_modules/systemjs/lib/extension-cjs.js',
       'node_modules/systemjs/lib/extension-register.js',
       'node_modules/angular2/node_modules/zone.js/zone.js',
-      'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js',
-      'node_modules/angular2/node_modules/rx/dist/rx.all.js'
+      'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js'
     ]
 };
 
@@ -52,18 +50,17 @@ gulp.task('libs', ['angular2'], function () {
 
 gulp.task('angular2', function () {
 
-    //transpile & concat
-    return gulp.src([
-            'node_modules/angular2/es6/prod/*.es6',
-            'node_modules/angular2/es6/prod/src/**/*.es6'],
-            { base: 'node_modules/angular2/es6/prod' })
-        .pipe(rename(function(path){
-            path.dirname = 'angular2/' + path.dirname; //this is not ideal... but not sure how to change angular's file structure
-            path.extname = ''; //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
-        })) 
-        .pipe(traceur({ modules: 'instantiate', moduleName: true}))
-        .pipe(concat('angular2.js'))
-        .pipe(gulp.dest('dist/lib'));
+  var buildConfig = {
+    paths: {
+      "angular2/*": "node_modules/angular2/es6/prod/*.es6",
+      "rx/*": "node_modules/angular2/node_modules/rx/*.js"
+    }
+  };
+
+  var Builder = require('systemjs-builder');
+  var builder = new Builder(buildConfig);
+
+  return builder.build('angular2/angular2', 'dist/lib/angular2.js', {});
 });
 
 gulp.task('play', ['default'], function () {
