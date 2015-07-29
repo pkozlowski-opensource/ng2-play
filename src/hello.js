4cf2ae3616e1b379477a03bfcdec0e39989732ce
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-07-29 17:20:52 +0200

    feat(alert): demo bs-alert directive

--- src/hello.js
+++ src/hello.js
@@ -1,18 +1,49 @@
-import {ComponentAnnotation as Component, ViewAnnotation as View, bootstrap, NgIf} from 'angular2/angular2';
+import {ComponentAnnotation as Component, ViewAnnotation as View, bootstrap, NgIf, EventEmitter} from 'angular2/angular2';
+
+@Component({
+    selector: 'bs-alert',
+    properties: ['type', 'dismissible'],
+    events: ['dismiss']
+})
+@View({
+    template: `<div attr.class="alert alert-{{_type}}" [class.alert-dismissible]="_dismissible" role="alert">
+                    <button *ng-if="_dismissible" type="button" class="close" aria-label="Close" (^click)="close()">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <ng-content></ng-content>
+                </div>`,
+    directives: [NgIf]
+})
+class BsAlert {
+    _availableTypes = ['success', 'info', 'warning', 'danger'];
+    _dismissible = false;
+    _type = this._availableTypes[2];
+    dismiss = new EventEmitter();
+
+    set type(val) {
+        this._type = this._availableTypes.indexOf(val) ? val : this._availableTypes[2];
+    }
+
+    set dismissible(val) {
+        this._dismissible = String(val) == "true";
+    }
+    
+    close() {
+        this.dismiss.next();
+    }
+}
 
 @Component({
     selector: 'hello'
 })
 @View({
-    template: `<span *ng-if="name">Hello, {{name}}!</span>`,
-    directives: [NgIf]
+    template: `<bs-alert [dismissible]="true" type="warning" (dismiss)="closed()">some content</bs-alert>`,
+    directives: [BsAlert]
 })
 export class Hello {
-    name: string = 'World';
-    constructor() {
-        setTimeout(() => {
-          this.name = 'NEW World'
-        }, 2000);
+    
+    closed() {
+        console.log('closed');
     }
 }
 
