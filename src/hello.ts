4ecd6a232ab22f9fb8049746ff8bb978a8c50579
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-10-09 10:33:45 +0200

    refactor: simplify the initial example

--- src/hello.ts
+++ src/hello.ts
@@ -1,10 +1,12 @@
-import {Component, View, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Component, bootstrap} from 'angular2/angular2';
 
-@Component({ selector: 'hello-app' })
-@View({template: `
-    <h1>Hello, {{name}}!</h1>
-    Say hello to: <input [value]="name" (input)="name = $event.target.value">
-` })
+@Component({
+    selector: 'hello-app',
+    template: `
+        <h1>Hello, {{name}}!</h1>
+        Say hello to: <input [value]="name" (input)="name = $event.target.value">
+    `
+})
 export class HelloApp {
     name: string = 'World';
 }
