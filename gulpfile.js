c499893ea24cb55904aa3ddfc86f5fd8e27c3461
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-11-04 17:18:06 +0100

    chore: move TypeScript config to tsconfig.json

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,11 @@ gulp.task('clean', function (done) {
 
 gulp.task('ts2js', function () {
     var typescript = require('gulp-typescript');
-    var tsResult = gulp.src(PATHS.src)
-        .pipe(typescript({
-            noImplicitAny: true,
-            module: 'system',
-            target: 'ES5',
-            moduleResolution: 'node',
-            emitDecoratorMetadata: true,
-            experimentalDecorators: true
-        }));
+    var tscConfig = require('./tsconfig.json');
+
+    var tsResult = gulp
+        .src(PATHS.src)
+        .pipe(typescript(tscConfig.compilerOptions));
 
     return tsResult.js.pipe(gulp.dest('dist'));
 });
