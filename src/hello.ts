df5da7ecd258f301a3973cc0ca0fe919a9dd382b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-11-06 16:14:06 +0100

    wip

--- src/hello.ts
+++ src/hello.ts
@@ -1,14 +1,44 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, Input, NgFor, ContentChild, TemplateRef, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'ngb-rating',
+    template: `
+        <div>
+            <template ng-for [ng-for-of]="ratings" [ng-for-template]="rateTpl" #active="rating < 5">
+                <span>X</span>
+            </template>
+        </div>
+    `,
+    directives: [NgFor]
+})
+class NgbRating {
+    private ratings: number[] = [];
+
+    @ContentChild(TemplateRef) rateTpl;
+
+    @Input()
+    rating: number;
+
+    @Input()
+    set maxRating(maxRating: number) {
+        this.ratings.length = maxRating;
+        for(var i=1; i<=maxRating; i++) {
+            this.ratings[i - 1] = i;
+        }
+    }
+}
+
 
 @Component({
     selector: 'hello-app',
     template: `
-        <h1>Hello, {{name}}!</h1>
-        Say hello to: <input [value]="name" (input)="name = $event.target.value">
-    `
+        <ngb-rating [max-rating]="5" [rating]="3">
+        </ngb-rating>
+    `,
+    directives: [NgbRating]
 })
-export class HelloApp {
-    name: string = 'World';
+class HelloApp {
+    rating = 6;
 }
 
 bootstrap(HelloApp);
