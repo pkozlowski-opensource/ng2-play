333c58e7ab197c8f072a198369527a9298bc3650
coryshaw1 <cory.shaw.dev@gmail.com>
2016-04-11 17:52:45 -0400

    Add bootstrap and font-awesome to get fuel-ui running correctly

--- src/hello.ts
+++ src/hello.ts
@@ -1,12 +1,19 @@
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
+import {DateRangePicker} from 'fuel-ui/fuel-ui';
 
 @Component({
     selector: 'hello-app',
     template: `
         <h1>Hello, {{name}}!</h1>
         Say hello to: <input [value]="name" (input)="name = $event.target.value">
-    `
+        <div class="row">
+            <div class="col-md-4">
+                <date-range-picker></date-range-picker>
+            </div>
+        </div>
+    `,
+    directives: [DateRangePicker]
 })
 export class HelloApp {
     name: string = 'World';
